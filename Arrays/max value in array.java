80714a03288aa11ac1f42b93c9945c8a4dead22a
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-16 10:45:23 +0530

    Update max value in array.java

--- Arrays/max value in array.java
+++ Arrays/max value in array.java
@@ -24,3 +24,29 @@ class Main
     }
 }
 
+/////////////////////////////  get maximum value in a range  //////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) 
+	{
+	   int[] arr = {1,2,77,466};
+      System.out.println(maxval(arr,1,3));
+	}
+    static int maxval(int[] arr,int start,int end){
+        int max_value = arr[0];
+        for(int i =start;i<=end;i++){
+            if(arr[i] > max_value){
+                max_value= arr[i];
+            }
+        }
+        return max_value;
+
+    }
+}
+

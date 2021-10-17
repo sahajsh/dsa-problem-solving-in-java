a94c8b16789ab254596ba9dfb2dbbe45d16588ae
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-17 15:39:35 +0530

    Update linear search in string.java

--- linear search/linear search in string.java
+++ linear search/linear search in string.java
@@ -26,3 +26,37 @@ class Main
 }
 
 output is false
+//-----------------------------------------------using for each loop----------------------------------------------------------------------
+	
+	
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) 
+	{
+		//linear search in string 
+        String name = "sahaj";
+        char target = 'q';
+        System.out.println(Arrays.toString(name.toCharArray()));
+        System.out.println(search(name,target));
+	}
+    static boolean search(String str, char target){
+        if(str.length() == 0){
+            return false;
+        }
+        for(char ch: str.toCharArray() ){
+            if(ch == target){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+//output-
+	//[s, a, h, a, j]
+       //   false

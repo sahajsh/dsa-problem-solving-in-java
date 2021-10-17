d13921389c23333031786992566ac103abcae538
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-10-17 14:25:06 +0530

    Update linear search in array.java

--- linear search/linear search in array.java
+++ linear search/linear search in array.java
@@ -30,3 +30,37 @@ class Main
 }
 
 output is 1
+	
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//code for linear search 
+        int[] nums = {1,2,3,5,44};
+        int target = 2;
+        int ans = linearSearch(nums,target);
+        System.out.println(ans);
+
+	}
+    static int linearSearch(int[] arr, int target){
+        if(arr.length==0){
+            return -1;
+        }
+        for(int element: arr){
+            
+            if(element== target){
+                return element;
+            }
+        }
+        return -1;
+    }
+}
+
+//it will return the element 2 
